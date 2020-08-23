342b33502fe780d38f151e401006e89a2f0df5cd
mmcd2886 <61208690+mmcd2886@users.noreply.github.com>
2020-08-23 18:50:29 -0400

    Update wikipedia_cik_ticker.py

--- wikipedia_cik_ticker.py
+++ wikipedia_cik_ticker.py
@@ -10,25 +10,23 @@
 import wikipedia as wp
 import pandas as pd
 
-#save the wikipedia page to 'html' variable. You only need to enter the end of the wikipedia site address. 
-#https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
+#enter the address for the wikipedia page and save to html variable
 html = wp.page("List_of_S%26P_500_companies").html().encode("UTF-8")
 
 #[] represents which table from the page you want to use. [0] is the first table on the wikipedia page and contains
 #sp500 information table that is needed. 
 df = pd.read_html(html)[0]
-#send the wikipedia table dataFrame to a csv
 df.to_csv(r'path_to_csv')
 
-#Extract just the second column of the CSV that contains the ticker symbols using 'usecols=[1]' and save to dataframe
+#Extract just the second column of the CSV that contains the ticker symbols using 'usecols=[1]'
 #Different columns can be extracted depending on what's needed
 dfTicker = pd.read_csv("path_to_csv", usecols=[1])
-#output  ticker symbols to a csv. Remove the header which says 'Symbol' and the index which numbers the rows
+
+#Remove the header which says 'Symbol' and the index which numbers the rows
 dfTicker.to_csv(r'path_to_csv', header=False, index=False)
 
-#extract the CIK number from the csv file
+#extract the CIK number from the csv file and output to csv. Remove the header 'CIK' and the index which numbers the rows
 dfCIK = pd.read_csv("path_to_csv", usecols=[8])
-#output CIK numbers to a csv. Remove the header 'CIK' and the index which numbers the rows
 dfCIK.to_csv(r'path_to_csv', header=False, index=False)
 
 
